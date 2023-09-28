79ddc08d4ad59e57d1ad3f72541a03227bb42064
bryce <brycecancode@gmail.com>
2023-09-28 22:27:28 +0545

    Added Comming Soon Data

--- app/components/header/account-setting/data-source-page/index.tsx
+++ app/components/header/account-setting/data-source-page/index.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from 'react-i18next'
+import cn from 'classnames'
 import DataSourceNotion from './data-source-notion'
+import s from './style.module.css'
 
 export default function DataSourcePage() {
   const { t } = useTranslation()
@@ -10,6 +12,32 @@ export default function DataSourcePage() {
     <div className='mb-8'>
       <div className='mb-2 text-sm font-medium text-gray-900'>{t('common.dataSource.add')}</div>
       <DataSourceNotion workspaces={[]} />
+      <div className='mb-2 border-[0.5px] border-gray-200 bg-gray-50 rounded-xl'>
+        <div className='flex items-center px-3 py-[9px]'>
+          <div className={cn(s['github-icon'], 'w-8 h-8 mr-3 border border-gray-100 rounded-lg')} />
+          <div className='grow'>
+            <div className='leading-5 text-sm font-medium text-gray-800'>
+              Github
+            </div>
+            <div className='leading-5 text-sm font-medium text-gray-200'>
+              Comming Soon
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='mb-2 border-[0.5px] border-gray-200 bg-gray-50 rounded-xl'>
+        <div className='flex items-center px-3 py-[9px]'>
+          <div className={cn(s['slack-icon'], 'w-8 h-8 mr-3 border border-gray-100 rounded-lg')} />
+          <div className='grow'>
+            <div className='leading-5 text-sm font-medium text-gray-800'>
+              Slack
+            </div>
+            <div className='leading-5 text-sm font-medium text-gray-200'>
+              Comming Soon
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
